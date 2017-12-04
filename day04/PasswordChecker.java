c6ca3b8a83d3843ddbb9bc0420f67b8d1878d38b
Oliver Scherf <oliver.scherf@udo.edu>
2017-12-04 23:21:02 +0100

    Finished day4 part B

--- day04/PasswordChecker.java
+++ day04/PasswordChecker.java
@@ -3,15 +3,19 @@ package day04;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class PasswordChecker {
 
 	public static void main(String[] args) {
+		partA();
+		partB();
+	}
+
+	private static void partA() {
 		HashSet<String> wordsSet = new HashSet<>();
-		
 		File file = new File("input.txt");
-		System.out.println(file.getAbsolutePath());
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 			int validPassPhrases = 0;
 			for (String line; (line = br.readLine()) != null;) {
@@ -31,12 +35,42 @@ public class PasswordChecker {
 				}
 				wordsSet.clear();
 			}
-			System.out.println("Valid Passphrases: " + validPassPhrases);
+			System.out.println("Valid Passphrases Part A: " + validPassPhrases);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 
+	private static void partB() {
+		HashSet<String> wordsSet = new HashSet<>();
+		File file = new File("input.txt");
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			int validPassPhrases = 0;
+			for (String line; (line = br.readLine()) != null;) {
+				String[] wordsOnLine = line.split(" ");
+				boolean valid = true;
+				for (String word : wordsOnLine) {
+					char[] sortedWord = word.toCharArray(); 
+					Arrays.sort(sortedWord);
+					
+					if (!wordsSet.contains(String.valueOf(sortedWord))) {
+						wordsSet.add(String.valueOf(sortedWord));
+					} else {
+						System.out.println("DUPLICATE: " + String.valueOf(sortedWord));;
+						valid = false;
+						break;
+					}
+				}
+				if (valid) {
+					validPassPhrases++;				
+				}
+				wordsSet.clear();
+			}
+			System.out.println("Valid Passphrases Part B: " + validPassPhrases);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
