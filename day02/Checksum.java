205af4722c2565248b95aea063c04283a10669ed
Oliver Scherf <oliver.scherf@udo.edu>
2017-12-04 23:54:24 +0100

    Finished day2 part B

--- day02/Checksum.java
+++ day02/Checksum.java
@@ -7,6 +7,11 @@ import java.util.Arrays;
 
 public class Checksum {
 	public static void main(String[] args) {
+		partA();
+		partB();
+	}
+
+	private static void partA() {
 		int sum = 0;
 		File file = new File("input.txt");
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -15,16 +20,51 @@ public class Checksum {
 				int[] numbers = new int[numbersString.length];
 				for (int i = 0; i < numbersString.length; ++i) {
 					numbers[i] = Integer.parseInt(numbersString[i]);
-					System.out.print(numbers[i]);
-					System.out.print(" ");
 				}
 				sum += Arrays.stream(numbers).max().getAsInt() - Arrays.stream(numbers).min().getAsInt();
-				System.out.println();
 			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(sum);
+		System.out.println("Sum Part A: " + sum);
+	}
+	
+	private static void partB() {
+		int sum = 0;
+		File file = new File("input.txt");
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			for (String line; (line = br.readLine()) != null;) {
+				String[] numbersString = line.split("\t");
+				int[] numbers = new int[numbersString.length];
+				for (int i = 0; i < numbersString.length; ++i) {
+					numbers[i] = Integer.parseInt(numbersString[i]);
+				}
+				for (int i = 0; i < numbers.length; ++i) {
+					boolean found = false;
+					for (int j = 0; j < numbers.length; ++j) {
+						if (i != j) {
+							if (numbers[i] % numbers[j] == 0) {
+								sum += numbers[i] / numbers[j];
+								found = true;
+								break;
+							}
+							if (numbers[j] % numbers[i] == 0) {
+								sum += numbers[j] / numbers[i];
+								found = true;
+								break;
+							}
+						}
+					}
+					if (found) {
+						break;
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Sum Part B: " + sum);
 	}
 }
