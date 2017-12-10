a9540248770d2f748e2a6a4a0f44023f77652990
OliverScherf <oliver.scherf@udo.edu>
2017-12-10 15:46:04 +0100

    Finished day8 part B

--- day08/RegisterInstructions.kt
+++ day08/RegisterInstructions.kt
@@ -6,14 +6,42 @@ import java.io.File
 val registers = mutableMapOf<String, Int>()
 
 fun main(args: Array<String>) {
-	val inputStream: InputStream = File("input.txt").inputStream()
 	var test = sequenceOf("b inc 5 if a > 1", "a inc 1 if b < 5", "c dec -10 if a >= 1", "c inc -20 if c == 10")
 	partA(test)
-	
+	var inputStream: InputStream = File("input.txt").inputStream()
 	inputStream.bufferedReader().useLines { lines ->
 		partA(lines)
 	}
+	
+	registers.clear()
+	inputStream = File("input.txt").inputStream()
+	partB(test)
+	inputStream.bufferedReader().useLines { lines ->
+		partB(lines)
+	}
+}
 
+fun partB(lines: Sequence<String>) {
+	if (true) {
+		
+	}
+	var max = 0
+	lines.asIterable()
+			.map { it.split(' ') }
+			.forEach {
+				if (condition(it.subList(4, 7))) {
+					registers.putIfAbsent(it[0], 0)
+					if (it[1].equals("inc")) {
+						registers += it[0] to (registers[it[0]]!!.toInt() + it[2].toInt())
+					} else {
+						registers += it[0] to (registers[it[0]]!!.toInt() - it[2].toInt())
+					}
+					if (max < registers[it[0]]!!.toInt()) {
+						max = registers[it[0]]!!.toInt() 
+					}
+				}
+			}
+	println(max)
 }
 
 fun partA(lines: Sequence<String>) {
@@ -22,7 +50,6 @@ fun partA(lines: Sequence<String>) {
 			.forEach {
 				if (condition(it.subList(4, 7))) {
 					registers.putIfAbsent(it[0], 0)
-					registers.putIfAbsent(it[0], 4)
 					if (it[1].equals("inc")) {
 						registers += it[0] to (registers[it[0]]!!.toInt() + it[2].toInt())
 					} else {
@@ -37,7 +64,6 @@ fun condition(cond: List<String>): Boolean {
 	val reg = cond[0]
 	registers.putIfAbsent(reg, 0)
 	val value = registers[reg]!!.toInt()
-	println(value)
 	when (cond[1]) {
 		">"  -> return value > cond[2].toInt()
 		">=" -> return value >= cond[2].toInt()
